b6a1de124783176ec13363478c0173f2b40fa63f
ShutoYamada <stym1122@gmail.com>
2023-05-08 17:59:07 +0900

    メインのコードを追加

--- bin/aws-s3-react-template.ts
+++ bin/aws-s3-react-template.ts
@@ -3,19 +3,41 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AwsS3ReactTemplateStack } from '../lib/aws-s3-react-template-stack';
 
+export type Env = {
+  AWS_ACCOUNT_ID: string;
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  AWS_DEFAULT_REGION: string;
+  S3_BUCKET_NAME: string;
+  CODECOMMIT_REPOSITORY_NAME: string;
+};
+
+const {
+  AWS_ACCOUNT_ID,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_DEFAULT_REGION,
+  S3_BUCKET_NAME,
+  CODECOMMIT_REPOSITORY_NAME
+} = process.env as Env;
+
+// Validate Parameters.
+if (
+  !AWS_ACCOUNT_ID ||
+  !AWS_ACCESS_KEY_ID ||
+  !AWS_SECRET_ACCESS_KEY ||
+  !AWS_DEFAULT_REGION ||
+  !S3_BUCKET_NAME ||
+  !CODECOMMIT_REPOSITORY_NAME
+)
+  throw new Error('The required parameter has not been specified.');
+
 const app = new cdk.App();
 new AwsS3ReactTemplateStack(app, 'AwsS3ReactTemplateStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: AWS_ACCOUNT_ID,
+    region: AWS_DEFAULT_REGION
+  },
+  s3BucketName: S3_BUCKET_NAME,
+  codeCommitRepoName: CODECOMMIT_REPOSITORY_NAME
 });
--- lib/aws-s3-react-template-stack.ts
+++ lib/aws-s3-react-template-stack.ts
@@ -1,16 +1,93 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { BlockPublicAccess, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
+
+interface AwsS3ReactTemplateStackProps extends cdk.StackProps {
+  /** S3 Bucket Name */
+  s3BucketName: string;
+  /** CodeCommit Repository Name */
+  codeCommitRepoName: string;
+}
 
 export class AwsS3ReactTemplateStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: AwsS3ReactTemplateStackProps
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create S3 bucket.
+    const s3Bucket = new s3.Bucket(this, 'MyS3Bucket', {
+      bucketName: props.s3BucketName,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      //blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsS3ReactTemplateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Set a policy to publish an S3 bucket as a static site.
+    s3Bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.AnyPrincipal()],
+        actions: ['s3:GetObject'],
+        resources: [s3Bucket.arnForObjects('*')]
+      })
+    );
+
+    // Create a CodeCommit repository
+    const codeCommitRepo = new codecommit.Repository(this, 'MyCodeCommitRepo', {
+      repositoryName: props.codeCommitRepoName
+    });
+
+    // Create a CodePipeline.
+    const pipeline = new codepipeline.Pipeline(this, 'MyPipeline');
+
+    // Add action to get source from CodeCommit.
+    const sourceOutput = new codepipeline.Artifact();
+    const sourceAction = new codepipeline_actions.CodeCommitSourceAction({
+      actionName: 'Source',
+      repository: codeCommitRepo,
+      output: sourceOutput,
+      branch: 'master'
+    });
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [sourceAction]
+    });
+
+    // Add action to deploy to S3 bucket.
+    const deployAction = new codepipeline_actions.S3DeployAction({
+      actionName: 'Deploy',
+      input: sourceOutput,
+      bucket: s3Bucket
+    });
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [deployAction]
+    });
+
+    // Output CodeCommit repository information.
+    new cdk.CfnOutput(this, 'CodeCommitCloneUrlHttp', {
+      value: codeCommitRepo.repositoryCloneUrlHttp
+    });
+    new cdk.CfnOutput(this, 'CodeCommitCloneUrlSsh', {
+      value: codeCommitRepo.repositoryCloneUrlSsh
+    });
+    new cdk.CfnOutput(this, 'CodeCommitUsername', {
+      value: codeCommitRepo.repositoryCloneUrlHttp.split('//')[1].split(':')[0]
+    });
+    new cdk.CfnOutput(this, 'CodeCommitPassword', {
+      value: codeCommitRepo.repositoryCloneUrlHttp.split('//')[1].split(':')[1]
+    });
   }
 }
